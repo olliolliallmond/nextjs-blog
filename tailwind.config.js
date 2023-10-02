6b849d86de9533ecc97d4bdec8faecbf25a82ea6
Amberlee Allmond <aallmondcar@gmail.com>
2023-10-02 09:29:09 -0400

    updated tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      colors: {
+        dark: "#1b1b1b",
+        light: "#fff",
+        accent: "#7B00D3", 
+        accentDark: "#ffdb4d",
+        gray: "#747474",
       },
     },
   },
@@ -20,9 +27,9 @@ module.exports = {
 
 
 // colors: {
-//   dark: "#1b1b1b",
-//   light: "#fff",
-//   accent: "#7B00D3", 
-//   accentDark: "#ffdb4d",
-//   gray: "#747474",
+  // dark: "#1b1b1b",
+  // light: "#fff",
+  // accent: "#7B00D3", 
+  // accentDark: "#ffdb4d",
+  // gray: "#747474",
 // }
