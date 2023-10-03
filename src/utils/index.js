ebf0d948f5fa62aeb2e5d1e464a9bd268045acaa
olliolliallmond <aallmondcar@gmail.com>
2023-10-03 17:21:27 -0400

    created util fxn to sort blogs

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,16 @@
+import { compareDesc, parseISO } from 'date-fns'
 
 export const cx = (...classNames) => classNames.filter(Boolean).join(" ")
+/** Notes:
+ * cx fxn to add mutliple classes instead of dependencies
+ * accepts classNames as an argument
+ * filter out flasey values from classNames (0, null, undefined, ... )
+ * join truthy values with " " (single space)
+ */
 
-// cx fxn to add mutliple classes instead of dependencies
-// accepts classNames as an argument
-// filter out flasey values from classNames (0, null, undefined, ... )
-// join truthy values with " " (single space)
+export const sortBlogs = (blogs) => {
+  return blogs.slice().sort((a,b) => compareDesc(parseISO(a.publishedAt), parseISO(b.publishedAt)))
+}
+/** Notes:
+ * sortBlogs fxn returns an array that is sorted from latest date to oldest date
+ */
