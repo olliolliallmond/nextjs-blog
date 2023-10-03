9b67c79ec8c2c6d39aebbcdabbb5fb05481b42d3
olliolliallmond <aallmondcar@gmail.com>
2023-10-03 11:30:31 -0400

    updated to include tailwind fonts and themes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
+import { cx } from '@/utils'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Manrope } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], display: "swap", variable:"--font-in" })
+
+const manrope = Manrope({ subsets: ['latin'], display: "swap", variable:"--font-mr" })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +14,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/* 
+        > Using Tailwind CSS, pass font variables into className for access by all children 
+        > add to className -- !! These are defined in `tailwind.config.js` !!
+          > `font-mr` => default font
+          > `bg-light` => default bg color
+          > `dark:bg-dark` => dark theme bg color 
+        > To avoid having to call `${inter.variable}` or `${manrope.variable}`
+          > create new folder and new file: `src/utils/index.js`
+          > created cx function
+        > Original: <body className={`${inter.variable} ${manrope.variable} font-mr bg-light dark:bg-dark`}>{children}</body>
+        > Alternative: <body className={cx(inter.variable, manrope.variable, "font-mr bg-light dark:bg-dark`")}>{children}</body>
+      */}
+      <body className={cx(inter.variable, manrope.variable, "font-mr bg-light dark:bg-dark`")}>{children}</body>
     </html>
   )
 }
