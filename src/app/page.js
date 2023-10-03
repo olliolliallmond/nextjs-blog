18be147ccd522fbb461ec99d862360a14a812938
olliolliallmond <aallmondcar@gmail.com>
2023-10-03 16:36:15 -0400

    confirmed generated content is accessible

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
-import Image from 'next/image'
+// import { allBlogs } from '@/.contentlayer/generated'
+import { allBlogs } from 'contentlayer/generated'
 
 export default function Home() {
+  console.log('All Blogs', allBlogs)
   return (
     <main className="flex flex-col items-center justify-center">
       Hello tiny humans
