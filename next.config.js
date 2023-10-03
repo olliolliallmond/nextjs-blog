d092ebbfeaab028230f48cdebb0369d0dd28f96a
olliolliallmond <aallmondcar@gmail.com>
2023-10-03 14:30:38 -0400

    Updated for contentlayer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+// Wrap NextJS config object in `withContentLayer` utility
+// Modify code from docs from ES6+ to CommonJS import/export
+
+const { withContentlayer } = require('next-contentlayer')
+
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withContentlayer({ ...nextConfig })
+
