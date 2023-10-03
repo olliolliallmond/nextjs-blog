6707bda9f646f39dc0cca1deffc609a30bacd416
olliolliallmond <aallmondcar@gmail.com>
2023-10-03 17:53:59 -0400

    updated overlay, styled description

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -14,7 +14,7 @@ const HomeCoverSection = ({blogs}) => {
     <div className="w-full inline-block">
       <article className='flex flex-col items-start justify-end mx-10 relative h-[85vh]'>
         <div className="absolute top-0 right-0 bottom-0 left-0 h-full 
-        bg-gradient-to-b from-transparent from-0% to-dark rounded-3xl z-0 " />
+        bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0 " />
         <Image src={blog.image.filePath.replace("../public", "")}
         placeholder='blur'
         blurDataURL={blog.image.blurhashDataUrl}
@@ -23,7 +23,7 @@ const HomeCoverSection = ({blogs}) => {
         className='w-full h-full object-center object-cover rounded-3xl -z-10'
         />
         <div className="w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light">
-          {/* <Link href={`/categories/${blog.tags[0]}`}>{blog.tags[0]}</Link> */}
+          
           <Tag link={`/categories/${blog.tags[0]}`} name={blog.tags[0]}/>
 
           <Link href={blog.url} className='mt-6'>
@@ -35,8 +35,8 @@ const HomeCoverSection = ({blogs}) => {
               </span>
             </h1>
           </Link>
-          
-          <p>{blog.description}</p>
+
+          <p className='inline-block mt-4 text-xl font-in'>{blog.description}</p>
         </div>
       </article>
     </div>
