ca3a8c2043cf8e898b65326c1265b0dbab2b3007
olliolliallmond <aallmondcar@gmail.com>
2023-10-03 15:07:24 -0400

    Defined docType name, filePathPattern, and fields

---
+++ contentlayer.config.js
@@ -0,0 +1,46 @@
+import { makeSource, defineDocumentType } from '@contentlayer/source-files'
+
+const Blog = defineDocumentType(() => ({
+  name: 'Blog',
+  filePathPattern: '**/**/*.mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    publishAt: {
+      type: 'date',
+      required: true,
+    },
+    updatedAt: {
+      type: 'date',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    image: {
+      type: 'string',
+      required: true,
+    },
+    isPublished: {
+      type: 'boolean',
+      default: true,
+    },
+    author: {
+      type: 'string',
+      required: true,
+    },
+    tags: {
+      type: 'list',
+      of: { type: 'string' },
+    }
+  },
+}))
+
+
+export default makeSource({
+  /* options */
+  contentDirPath: 'content',
+})
