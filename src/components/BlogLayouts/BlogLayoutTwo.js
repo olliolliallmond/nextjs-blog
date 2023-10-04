c81a4c51b435c18665ad399aa8360aa8f292df71
olliolliallmond <aallmondcar@gmail.com>
2023-10-04 13:48:16 -0400

    Added comments, completed layout 2

--- src/components/BlogLayouts/BlogLayoutTwo.js
+++ src/components/BlogLayouts/BlogLayoutTwo.js
@@ -1,7 +1,44 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Tag from '../Elements/Tag'
+import { format } from 'date-fns'
 
-const BlogLayoutTwo = () => {
+const BlogLayoutTwo = ({blog}) => {
   return (
-    <div>BlogLayoutTwo</div>
+    <div className='grid grid-cols-12 gap-4 items-center text-dark' >
+      {/* Image as Link */}
+      <Link href={blog.url} className='col-span-4 h-full rounded-xl overflow-hidden'>
+        <Image src={blog.image.filePath.replace("../public", "")}
+          placeholder='blur'
+          blurDataURL={blog.image.blurhashDataUrl}
+          alt={blog.title}
+          width={blog.image.width}
+          height={blog.image.height}
+          className='aspect-square w-full h-full object-center object-cover'
+        />
+      </Link>
+
+      {/* Details */}
+      <div className="w-full col-span-8 ">
+        <span className='uppercase text-accent font-semibold text-sm'>{blog.tags[0]}</span>
+
+        <Link href={blog.url} className='inline-block my-1'>
+          <h2 className='font-semibold capitalize text-lg' >
+            <span className="bg-gradient-to-r from-accent/50 to-accent/50 bg-[length:0px_3px] 
+                              hover:bg-[length:100%_3px] bg-left-bottom bg-no-repeat 
+                              transition-[background-size] duration-500">
+              {blog.title}
+            </span>
+          </h2>
+        </Link>
+
+        {/* Date */}
+        <span className='capitalize text-dark/50 font-semibold text-base'>
+          { format(new Date(blog.publishedAt), "MMMM dd, yyyy") }
+        </span>
+      </div>
+
+    </div>
   )
 }
 
--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -13,8 +13,10 @@ const HomeCoverSection = ({blogs}) => {
   return (
     <div className="w-full inline-block">
       <article className='flex flex-col items-start justify-end mx-10 relative h-[85vh]'>
+        {/* Overlay */}
         <div className="absolute top-0 right-0 bottom-0 left-0 h-full 
         bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0 " />
+        {/* Blog image */}
         <Image src={blog.image.filePath.replace("../public", "")}
         placeholder='blur'
         blurDataURL={blog.image.blurhashDataUrl}
@@ -22,6 +24,7 @@ const HomeCoverSection = ({blogs}) => {
         fill
         className='w-full h-full object-center object-cover rounded-3xl -z-10'
         />
+        {/* Blog overview over image */}
         <div className="w-3/4 p-16 flex flex-col items-start justify-center z-0 text-light">
           
           <Tag link={`/categories/${blog.tags[0]}`} name={blog.tags[0]}/>
