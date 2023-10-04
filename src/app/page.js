dc7bc3b700d657e629343c8bb94a05aae80e86cf
olliolliallmond <aallmondcar@gmail.com>
2023-10-04 13:21:45 -0400

    Creating featured post blog layouts

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 // import { allBlogs } from '@/.contentlayer/generated'
 import { allBlogs } from 'contentlayer/generated'
 import HomeCoverSection from '../components/Home/HomeCoverSection'
+import FeatueredPosts from '../components/Home/FeatueredPosts'
 
 export default function Home() {
   // console.log('All Blogs', allBlogs)
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
+      <FeatueredPosts blogs={allBlogs} />
     </main>
   )
 }
---
+++ src/components/BlogLayouts/BlogLayoutTwo.js
@@ -0,0 +1,8 @@
+
+const BlogLayoutTwo = () => {
+  return (
+    <div>BlogLayoutTwo</div>
+  )
+}
+
+export default BlogLayoutTwo
